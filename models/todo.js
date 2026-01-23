9a921b671f64af912709750a5f913bf2ec757779
Zamar443 <Zamar443@users.noreply.github.com>
2026-01-23 16:47:11 +0100

    add

---
+++ models/todo.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema(
+  {
+    todoId: Number,
+    title: String,
+    completed: Boolean,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Todo", todoSchema);
